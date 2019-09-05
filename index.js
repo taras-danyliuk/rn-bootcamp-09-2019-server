112cd490695a9752d61b8d9d6f9bfaeddda60d65
Taras Danyliuk <taras.danylyuk@coaxsoft.com>
2019-09-05 11:35:26 +0300

    add file upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require("cors");;
+const cors = require("cors");
+const fileUpload = require('express-fileupload');
 const socketIO = require("socket.io");
 
 const userRouter = require("./routes/user");
 const eventRouter = require("./routes/event");
+const imageRouter = require("./routes/image");
 
 
 const app = express();
@@ -14,6 +16,7 @@ const dbPath = "mongodb://coax-bootcamp-admin:CO888777ax@ds143262.mlab.com:43262
 
 
 // middleware
+app.use(fileUpload({ useTempFiles: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -21,6 +24,7 @@ app.use(cors());
 // Set up routes
 app.use("/api/user", userRouter);
 app.use("/api/event", eventRouter);
+app.use("/api/image", imageRouter);
 
 
 // Start server
---
+++ routes/image.js
@@ -0,0 +1,32 @@
+const cloudinary = require('cloudinary').v2;
+const router = require("express").Router();
+const fs = require("fs");
+
+
+router.post("/upload", async(req, res) => {
+  try {
+    if (!req.files) {
+      res.status(400);
+
+      return res.send({ status: "error", error: "no file uploaded" });
+    }
+
+    const imageFile = req.files.image;
+    const result = await cloudinary.uploader.upload(imageFile.tempFilePath, { tags: "rn-bootcamp-09-2019" });
+
+    fs.unlink(imageFile.tempFilePath);
+
+    //send response
+    res.send({
+      status: "success",
+      imageUrl: result.url,
+    });
+
+  } catch (err) {
+    console.log(err, "error");
+    res.status(400);
+    res.send(err);
+  }
+});
+
+module.exports = router;
